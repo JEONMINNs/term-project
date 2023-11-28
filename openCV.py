374cb305aab870680f1b4b801180437e1ae9624a
Jinho-5 <ozno100@naver.com>
2023-11-28 17:55:19 +0900

    oznocommit

--- openCV.py
+++ openCV.py
@@ -1,35 +1,54 @@
+# -*- coding: utf-8 -*-import cv2
 import cv2
+import dlib
 import numpy as np
 
-# 웹캠 열기
-cap = cv2.VideoCapture(0)
+# Dlib의 얼굴 및 눈 감지기 초기화
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
-# 감지할 색상 범위 정의 (HSV 형식)
-lower_color = np.array([0, 0, 0])
-upper_color = np.array([150, 150, 150])
+# 카메라 열기
+cap = cv2.VideoCapture(0)  # 0은 기본 카메라를 의미합니다.
 
 while True:
     # 프레임 읽기
     ret, frame = cap.read()
-
-    # BGR을 HSV로 변환
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # 지정한 색상 범위에 해당하는 부분을 찾기
-    mask = cv2.inRange(hsv, lower_color, upper_color)
-
-    # 원본 이미지에서 색상에 해당하는 부분만 추출
-    result = cv2.bitwise_and(frame, frame, mask=mask)
-
-    # 결과 이미지 출력
-    cv2.imshow('Color Detection', result)
-
-    # 'q' 키를 누르면 종료
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if not ret:
         break
 
-# 사용한 자원 해제
-cap.release()
-cv2.destroyAllWindows()
+    # 그레이 스케일 변환
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#주석 처리를 추가해보는 ozno
+    # 얼굴 감지
+    faces = detector(gray)
+
+    for face in faces:
+        # 얼굴 전체 좌표
+        x, y, w, h = face.left(), face.top(), face.width(), face.height()
+
+        # 윗쪽 절반 좌표 계산
+        y_top = max(y, y + h // 3)
+
+        # 눈 감지
+        landmarks = predictor(gray, face)
+        for n in range(36, 48):  # 36부터 47까지는 눈 부분
+            x_eye = landmarks.part(n).x
+            y_eye = landmarks.part(n).y
+
+            # 눈 주변 좌표 계산
+            x1 = max(0, x_eye - 5)
+            y1 = max(0, y_eye - 5)
+            x2 = min(frame.shape[1], x_eye + 5)
+            y2 = min(frame.shape[0], y_eye + 5)
+
+            # 얼굴에서 눈을 제외한 주변 부분 모자이크 처리
+            if y1 < y2 and x1 < x2:
+                # 모자이크 처리할 부분
+                mosaic_region = frame[y_top:y + h, x:x + w]
+
+                # 모자이크 크기 지정
+                mosaic_size = (10, 10)
+
+                # 모자이크 적용
+                mosaic = cv2.resize(mosaic_region, mosaic_size, interpolation=cv2.INTER_NEAREST)
+                frame[y_top:y + h, x:x + w] = cv2.resize(mosaic, mosaic_region.shape[:2][::-1], interpolation=cv2.INTER_NEAREST)
